1f2eb3e029736d9eec43571d2fcdf6b75691e15a
sebastian golijow <sebastian@oneragtime.com>
2024-02-26 12:34:38 +0100

    make admin page work for add edit users

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,40 @@
+from django.contrib.auth import get_user_model
+from django.test import Client
+from django.test import TestCase
+from django.urls import reverse
+
+
+class AdimSiteTests(TestCase):
+
+    def setUp(self):
+        """Create user and client"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='superSecretPassword'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='superSecretPassword',
+            name='Test User'
+        )
+
+    def test_users_list(self):
+        """Test user listed on admin page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_edit_page(self):
+        """Test the edit user page in admin"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Create to create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
