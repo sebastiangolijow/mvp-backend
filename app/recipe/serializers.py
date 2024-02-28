70cc8fcd699a4ab818ab06f1f3eccd4163c1db01
sebastian golijow <sebastian@oneragtime.com>
2024-02-28 18:02:19 +0100

    functionalities for tag

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,3 +1,4 @@
+from core.models import Tag
 from core.models import Recipe
 from rest_framework import serializers
 
@@ -13,3 +14,10 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,9 +1,11 @@
+from core.models import Tag
+from core.models import Recipe
+from recipe import serializers
+from rest_framework import mixins
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
-from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.RecipeDetailSerializer
@@ -22,3 +24,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class TagViewSet(
+        mixins.DestroyModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.ListModelMixin,
+        viewsets.GenericViewSet
+    ):
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-name')
